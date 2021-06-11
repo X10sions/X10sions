fa88ac136de0ffe542e38f8731b62b65f7fe1194
travbeamo <63974155+travbeamo@users.noreply.github.com>
2021-06-11 15:29:42 +1000

    Update Constants.cs

--- src/x/xDamienBod/.AppAuthorizationService/Constants.cs
+++ src/x/xDamienBod/.AppAuthorizationService/Constants.cs
@@ -1,6 +1,6 @@
 ﻿namespace xDamienBod.AppAuthorizationService {
   public static class Constants {
-    public const string AdminUser = "MARTOGGCO\\TBeames";
+    public const string AdminUser = "DOMAIN\\USER";
 
     public const string IdentityProviderClaim = "http://schemas.microsoft.com/identity/claims/identityprovider";
     public const string IdentityProviderValue = "Windows";
