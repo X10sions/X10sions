3ce75479c1fec2d1c9bab0ad30eeea2132a5a4a8
tb-mtg <tbeames@martogg.com.au>
2022-03-11 15:40:29 +1100

    Updated nuget packages

--- src/x/xLinqToDB/DataProvider/_Generic/GenericSqlOptimizer.cs
+++ src/x/xLinqToDB/DataProvider/_Generic/GenericSqlOptimizer.cs
@@ -11,8 +11,8 @@ namespace LinqToDB.DataProvider {
     }
     DataSourceInformationRow dataSourceInformationRow;
 
-      public override ISqlExpression ConvertExpressionImpl<TContext>(ISqlExpression expression, ConvertVisitor<TContext> visitor, EvaluationContext context) { 
-      expression = base.ConvertExpressionImpl(expression, visitor, context);
+      public override ISqlExpression ConvertExpressionImpl(ISqlExpression expression, ConvertVisitor<RunOptimizationContext> visitor) { 
+      expression = base.ConvertExpressionImpl(expression, visitor);
       return dataSourceInformationRow.DataSourceProduct?.DbSystem?.Name switch {
         DbSystem.Names.DB2iSeries => ConvertExpressionImpl_DB2iSeries(expression),
         _ => expression
@@ -24,19 +24,17 @@ namespace LinqToDB.DataProvider {
 namespace LinqToDB.DataProvider.DB2iSeries {
 
   public class DB2iSeriesV5R4SqlOptimizer : _BaseSqlOptimizer {
-    public DB2iSeriesV5R4SqlOptimizer(SqlProviderFlags sqlProviderFlags) : base(sqlProviderFlags) {
-    }
+    public DB2iSeriesV5R4SqlOptimizer(SqlProviderFlags sqlProviderFlags) : base(sqlProviderFlags) {    }
 
-    public override ISqlExpression ConvertExpressionImpl<TContext>(ISqlExpression expression, ConvertVisitor<TContext> visitor, EvaluationContext context) {
-      expression = base.ConvertExpressionImpl(expression, visitor, context);
+    public override ISqlExpression ConvertExpressionImpl(ISqlExpression expression, ConvertVisitor<RunOptimizationContext> visitor) {
+      expression = base.ConvertExpressionImpl(expression, visitor );
       return ConvertExpressionImpl_DB2iSeries(expression);
     }
 
   }
 
   public class DB2iSeriesV7R4SqlOptimizer : DB2iSeriesV5R4SqlOptimizer {
-    public DB2iSeriesV7R4SqlOptimizer(SqlProviderFlags sqlProviderFlags) : base(sqlProviderFlags) {
-    }
+    public DB2iSeriesV7R4SqlOptimizer(SqlProviderFlags sqlProviderFlags) : base(sqlProviderFlags) {    }
   }
 
 }
