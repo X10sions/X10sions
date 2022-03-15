6d56ff4b48d5eb0cfd2f540a216e5138a87a2de8
tb-mtg <tbeames@martogg.com.au>
2022-03-15 22:25:47 +1100

    Added CompiledExpression

---
+++ src/Common/.NetStandard/Linq/CompiledExpression.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq.Expressions;
+//using FastExpressionCompiler;
+//using FastExpressionCompiler.LightExpression;
+//using static FastExpressionCompiler.LightExpression.Expression;
+
+namespace Common.Linq;
+
+public class CompiledExpression<T> {
+  public CompiledExpression(Expression<Func<T, bool>> predicateExpr) {
+    Expression = predicateExpr;
+    Func = predicateExpr.Compile();
+    //Func = predicateExpr.CompileFast();
+    //  predicateExpr.to
+  }
+
+  public CompiledExpression(Func<T, bool> predicateFunc) {
+    Expression = x => predicateFunc(x);
+    Func = predicateFunc;
+  }
+  public bool Value(T arg) => Func(arg);
+  public Expression<Func<T, bool>> Expression { get; }
+  public Func<T, bool> Func { get; }
+}
+
+//public class xxComputedBool<T> : xxComputed<T, bool> {
+//  public xxComputedBool(bool value) : base(value) { }
+//}
+
