b9d1d1bb146c44c8cd2ad194f93ac2f1a50c4594
tb-mtg <tbeames@martogg.com.au>
2022-03-30 17:11:10 +1100

    Added DbSytem & DbProvider enums & extension methods

---
+++ src/Common/.Abstractions/Data/DbProviderOption.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace Common.Data;
+
+public enum DbProviderOption {
+  Devart_Data_MySql,
+  Devart_Data_Oracle,
+  Devart_Data_PostgreSql,
+  Devart_Data_SQLite,
+  FirebirdSql_Data_FirebirdClient,
+  IBM_Data_DB2,
+  IBM_Data_DB2_Core,
+  IBM_Data_DB2_iSeries,
+  Microsoft_Data_Sqlite,
+  Microsoft_Data_SqlClient,
+  MongoDB_Driver,
+  MySql_Data_MySqlClient,
+  MySqlConnector,
+  Oracle_ManagedDataAccess_Client,
+  Npgsql,
+  Redis,
+  System_Data_Odbc,
+  System_Data_OleDb,
+  System_Data_OracleClient,
+  System_Data_SQLite,
+  System_Data_SqlClient
+}
--- src/Common/.Abstractions/Data/IConnectionString.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Common.Data;
-  public interface IConnectionString {
-  // https://github.com/serenity-is/Serenity/blob/master/src/Serenity.Net.Data/Connections/IConnectionString.cs
-
-  //ISqlDialect Dialect { get; }
-  string? Key { get; }
-  string Value { get; }
-  string? ProviderName { get; }
-}
