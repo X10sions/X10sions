c5b8fa0220dfb27d087b5c7a816957afb359b784
tb-mtg <tbeames@martogg.com.au>
2022-05-19 15:19:15 +1000

    Removed Obsolete predicate Expression Extensions methods

--- src/x/xSystem/Linq/Expressions/ExpressionExtensions.cs
+++ src/x/xSystem/Linq/Expressions/ExpressionExtensions.cs
@@ -40,11 +40,6 @@ public static class ExpressionExtensions {
         nameof(Queryable.ThenByDescending)
     };
 
-  //public static Expression<Func<TSource, TDestination>> Combine<TSource, TDestination>(this IEnumerable<Expression<Func<TSource, TDestination>>> selectors) => selectors.ToArray().Combine();
-
-  //public static Expression<Func<TSource, TDestination>> Combine<TSource, TDestination>(this Expression<Func<TSource, TDestination>> firstSelector, params Expression<Func<TSource, TDestination>>[] OtherSelectors) => (new[] { firstSelector }).Union(OtherSelectors).Combine();
-
-
   public static bool IsExpressionBodyConstant<T>(this Expression<Func<T, bool>> expr) => expr.Body.NodeType == ExpressionType.Constant;
 
   public static bool IsOrderingMethod(this Expression expression) => queryableOrderMethods.Any(method => IsQueryableMethod(expression, method));
@@ -113,20 +108,6 @@ public static class ExpressionExtensions {
     return Expression.Lambda<Func<T, bool>>(Expression.AndAlso(replace.left, replace.right), replace.parameter);
   }
 
-  [Obsolete("Try Remove")] public static Expression<Func<T, bool>> AndAlso_Akunzai<T>(this Expression<Func<T, bool>> left, Expression<Func<T, bool>> right)    => left == null ? right : Expression.Lambda<Func<T, bool>>(Expression.AndAlso(left.Body, right.Body), left.Parameters.Single());
-
-  [Obsolete("Try Remove")]
-  public static Expression<Func<T, bool>> AndAlso_ISPO<T>(this Expression<Func<T, bool>> left, Expression<Func<T, bool>> right) {
-    if (left.Equals(right)) return left;
-    if (left == null || left.Equals(True<T>())) return right;
-    if (right == null || right.Equals(True<T>())) return left;
-    if (left.Equals(False<T>()) || right.Equals(False<T>())) return False<T>();
-    return Expression.Lambda<Func<T, bool>>(Expression.AndAlso(left.Body, right.Body.Replace(right.Parameters[0], left.Parameters.Single())), left.Parameters);
-  }
-
-  [Obsolete("Try Remove")] public static Expression<Func<T, bool>> AndAlso_PE<T>(this Expression<Func<T, bool>> left, Expression<Func<T, bool>> right) => ReplaceExpressions_PE(left, right, ExpressionType.AndAlso);
-  [Obsolete("Try Remove")] public static Expression<Func<T, bool>> AndAlso_UPB<T>(this Expression<Func<T, bool>> first, Expression<Func<T, bool>> second) => ReplaceExpressions_UPB(first, second, Expression.AndAlso);
-
   public static Expression<Func<T, bool>> AndAlsoIf<T>(this Expression<Func<T, bool>> expr1, bool? ifTrue, Expression<Func<T, bool>> truePredicate) => ifTrue.HasValue && ifTrue.Value ? expr1.AndAlso(truePredicate) : expr1;
   public static Expression<Func<T, bool>> AndAlsoIf<T>(this Expression<Func<T, bool>> expr1, bool? ifTrue, Expression<Func<T, bool>> truePredicate, Expression<Func<T, bool>> falsePredicate) => expr1.AndAlso(ifTrue.HasValue && ifTrue.Value ? truePredicate : falsePredicate);
   public static Expression<Func<T, bool>> Equal<T, TProp>(this Expression<Func<T, TProp>> field, TProp value) => Expression.Lambda<Func<T, bool>>(Expression.Equal(field.Body, Expression.Constant(value, typeof(TProp))), field.Parameters);
@@ -137,7 +118,6 @@ public static class ExpressionExtensions {
   public static Expression<Func<T, bool>> LessThan<T, TProp>(this Expression<Func<T, TProp>> field, TProp value) => Expression.Lambda<Func<T, bool>>(Expression.LessThan(field.Body, Expression.Constant(value, typeof(TProp))), field.Parameters);
   public static Expression<Func<T, bool>> LessThanOrEqual<T, TProp>(this Expression<Func<T, TProp>> field, TProp value) => Expression.Lambda<Func<T, bool>>(Expression.LessThanOrEqual(field.Body, Expression.Constant(value, typeof(TProp))), field.Parameters);
   public static Expression<Func<T, bool>> Not<T>(this Expression<Func<T, bool>> expression) => Expression.Lambda<Func<T, bool>>(Expression.Not(expression.Body), expression.Parameters);
-  //public static Expression<Func<T, bool>> Not<T>(this Expression<Func<T, bool>> expression) => Expression.Lambda<Func<T, bool>>(Expression.Not(expression.Body), expression.Parameters[0]);
   public static Expression<Func<T, bool>> NotEqual<T, TProp>(this Expression<Func<T, TProp>> field, TProp value) => Expression.Lambda<Func<T, bool>>(Expression.NotEqual(field.Body, Expression.Constant(value, typeof(TProp))), field.Parameters);
   public static Expression<Func<T, bool>> NotEqualNull<T, TProp>(this Expression<Func<T, TProp?>> field) where TProp : struct => Expression.Lambda<Func<T, bool>>(Expression.NotEqual(field.Body, Expression.Constant(null, typeof(TProp?))), field.Parameters);
   public static Expression<Func<T, bool>> OrElse<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2) {
@@ -148,19 +128,6 @@ public static class ExpressionExtensions {
     var replace = expr1.ReplaceExpressions(expr2);
     return Expression.Lambda<Func<T, bool>>(Expression.OrElse(replace.left, replace.right), replace.parameter);
   }
-  [Obsolete("Try Remove")] public static Expression<Func<T, bool>> OrElse_Akunzai<T>(this Expression<Func<T, bool>> left, Expression<Func<T, bool>> right)    => left == null ? right : Expression.Lambda<Func<T, bool>>(Expression.OrElse(left.Body, right.Body), left.Parameters.Single());
-
-  [Obsolete("Try Remove")]
-  public static Expression<Func<T, bool>> OrElse_ISPU<T>(this Expression<Func<T, bool>> left, Expression<Func<T, bool>> right) {
-    if (left.Equals(right)) return left;
-    if (left == null || left.Equals(False<T>())) return right;
-    if (right == null || right.Equals(False<T>())) return left;
-    if (left.Equals(True<T>()) || right.Equals(True<T>())) return True<T>();
-    return Expression.Lambda<Func<T, bool>>(Expression.OrElse(left.Body, right.Body.Replace(right.Parameters[0], left.Parameters.Single())), left.Parameters);
-  }
-
-  [Obsolete("Try Remove")] public static Expression<Func<T, bool>> OrElse_PE<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2) => ReplaceExpressions_PE(expr1, expr2, ExpressionType.OrElse);
-  [Obsolete("Try Remove")] public static Expression<Func<T, bool>> OrElse_UPB<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2) => ReplaceExpressions_UPB(expr1, expr2, Expression.OrElse);
 
   public static Expression<Func<T, bool>> OrElseIf<T>(this Expression<Func<T, bool>> expr1, bool? ifTrue, Expression<Func<T, bool>> truePredicate) => ifTrue.HasValue && ifTrue.Value ? expr1.OrElse(truePredicate) : expr1;
   public static Expression<Func<T, bool>> OrElseIf<T>(this Expression<Func<T, bool>> expr1, bool? ifTrue, Expression<Func<T, bool>> truePredicate, Expression<Func<T, bool>> falsePredicate) => expr1.OrElse(ifTrue.HasValue && ifTrue.Value ? truePredicate : falsePredicate);
@@ -178,42 +145,17 @@ public static class ExpressionExtensions {
     return (new ReplaceExpressionVisitor(expr1.Parameters[0], parameter).Visit(expr1.Body), new ReplaceExpressionVisitor(expr2.Parameters[0], parameter).Visit(expr2.Body), parameter);
   }
 
-  [Obsolete("Try Remove")]
-  private static Expression<Func<T, bool>> ReplaceExpressions_PE<T>(this Expression<Func<T, bool>> left, Expression<Func<T, bool>> right, ExpressionType expressionType) {
-    if (IsExpressionBodyConstant(left)) return right;
-    var p = left.Parameters[0];
-    var visitor = new ReplaceParameterExpressionVisitor(p, right.Parameters[0]);
-    return Expression.Lambda<Func<T, bool>>(Expression.MakeBinary(expressionType, left.Body, visitor.Visit(right.Body)), new ParameterExpression[1] {
-      p
-    });
-  }
-
-  [Obsolete("Try Remove")]
-  private static Expression<T> ReplaceExpressions_UPB<T>(this Expression<T> first, Expression<T> second, Func<Expression, Expression, Expression> merge) {
-    var map = first.Parameters.Select((f, i) => new { f, s = second.Parameters[i] }).ToDictionary((p) => p.s, (p) => p.f);
-    return Expression.Lambda<T>(merge(first.Body, new ReplaceParameterExpressionVisitor(map).Visit(second.Body)), first.Parameters);
-  }
-
   public static Expression<Func<TNewParam, TResult>> ReplaceParameter<TNewParam, TOldParam, TResult>(this Expression<Func<TOldParam, TResult>> expression) where TNewParam : TOldParam {
     var param = Expression.Parameter(typeof(TNewParam));
     return Expression.Lambda<Func<TNewParam, TResult>>(expression.Body.Replace(expression.Parameters[0], param), param);
   }
 
   // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/generics/constraints-on-type-parameters
-  //public static Expression<Func<T2, T1, TResult>> SwapParameters00<T1, T2, TResult>(this Expression<Func<T1, T2, TResult>> exprFunc) => Expression.Lambda<Func<T2, T1, TResult>>(exprFunc.Body, exprFunc.Parameters[1], exprFunc.Parameters[0]);
-  //public static Expression<Func<T2, T1?, TResult>> SwapParameters01<T1, T2, TResult>(this Expression<Func<T1, T2?, TResult>> exprFunc) => Expression.Lambda<Func<T2, T1?, TResult>>(exprFunc.Body, exprFunc.Parameters[1], exprFunc.Parameters[0]);
-  //public static Expression<Func<T2?, T1, TResult>>  SwapParameters10<T1, T2, TResult>(this Expression<Func<T1?, T2, TResult>> exprFunc)  => Expression.Lambda<Func<T2?, T1, TResult>>(exprFunc.Body, exprFunc.Parameters[1], exprFunc.Parameters[0]);
-  //public static Expression<Func<T2?, T1?, TResult>> SwapParameters11<T1, T2, TResult>(this Expression<Func<T1?, T2?, TResult>> exprFunc)  => Expression.Lambda<Func<T2?, T1?, TResult>>(exprFunc.Body, exprFunc.Parameters[1], exprFunc.Parameters[0]);
 
   public static Expression<Func<T2, T1, TResult>> SwapParameters00<T1, T2, TResult>(this Expression<Func<T1, T2, TResult>> exprFunc) where T1 : class where T2 : class => Expression.Lambda<Func<T2, T1, TResult>>(exprFunc.Body, exprFunc.Parameters[1], exprFunc.Parameters[0]);
   public static Expression<Func<T2, T1, TResult>> SwapParameters01<T1, T2, TResult>(this Expression<Func<T1, T2, TResult>> exprFunc) where T1 : class where T2 : class? => Expression.Lambda<Func<T2, T1, TResult>>(exprFunc.Body, exprFunc.Parameters[1], exprFunc.Parameters[0]);
   public static Expression<Func<T2, T1, TResult>> SwapParameters10<T1, T2, TResult>(this Expression<Func<T1, T2, TResult>> exprFunc) where T1 : class? where T2 : class => Expression.Lambda<Func<T2, T1, TResult>>(exprFunc.Body, exprFunc.Parameters[1], exprFunc.Parameters[0]);
   public static Expression<Func<T2, T1, TResult>> SwapParameters11<T1, T2, TResult>(this Expression<Func<T1, T2, TResult>> exprFunc) where T1 : class? where T2 : class? => Expression.Lambda<Func<T2, T1, TResult>>(exprFunc.Body, exprFunc.Parameters[1], exprFunc.Parameters[0]);
-
-  //public static Expression<Func<T2, T1, TResult>> SwapParameters00<T1, T2, TResult>(this Expression<Func<T1, T2, TResult>> exprFunc) where T1 : notnull where T2 : notnull  => Expression.Lambda<Func<T2, T1, TResult>>(exprFunc.Body, exprFunc.Parameters[1], exprFunc.Parameters[0]);
-  //public static Expression<Func<T2, T1, TResult>> SwapParameters01<T1, T2, TResult>(this Expression<Func<T1, T2, TResult>> exprFunc) where T1 : notnull where T2 : T1? => Expression.Lambda<Func<T2, T1, TResult>>(exprFunc.Body, exprFunc.Parameters[1], exprFunc.Parameters[0]);
-  //public static Expression<Func<T2, T1, TResult>> SwapParameters10<T1, T2, TResult>(this Expression<Func<T1, T2, TResult>> exprFunc) where T1 : T1? where T2 : notnull => Expression.Lambda<Func<T2, T1, TResult>>(exprFunc.Body, exprFunc.Parameters[1], exprFunc.Parameters[0]);
-  //public static Expression<Func<T2, T1, TResult>> SwapParameters11<T1, T2, TResult>(this Expression<Func<T1, T2, TResult>> exprFunc) where T1 : T1? where T2 : T1? => Expression.Lambda<Func<T2, T1, TResult>>(exprFunc.Body, exprFunc.Parameters[1], exprFunc.Parameters[0]);
 
   public static Expression<Func<T2, T1, TResult>> SwapParameters1<T1, T2, TResult>(this Expression<Func<T1, T2, TResult>> exprFunc)
     => Expression.Lambda<Func<T2, T1, TResult>>(exprFunc.Body, exprFunc.Parameters[1], exprFunc.Parameters[0]);
--- src/x/xSystem/Linq/Expressions/_ExpressionVisitors/ReplaceExpressionVisitor.cs
+++ src/x/xSystem/Linq/Expressions/_ExpressionVisitors/ReplaceExpressionVisitor.cs
@@ -1,7 +1,6 @@
 ﻿namespace System.Linq.Expressions;
 
 public class ReplaceExpressionVisitor : ExpressionVisitor {
-  //public ReplaceExpressionVisitor() { }
   public ReplaceExpressionVisitor(Expression source, Expression target) {
     this.source = source;
     this.target = target;
@@ -11,43 +10,4 @@ public class ReplaceExpressionVisitor : ExpressionVisitor {
   private readonly Expression target;
 
   public override Expression Visit(Expression node) => node == source ? target : base.Visit(node);
-}
-
-[Obsolete("Try Remove")]
-public class ReplaceParameterExpressionVisitor : ExpressionVisitor {
-  public ReplaceParameterExpressionVisitor(ParameterExpression left, ParameterExpression right) {
-    Expressions[right] = left;
-    codeSource = CodeSource.PredicateExtensions;
-  }
-
-  public ReplaceParameterExpressionVisitor(Dictionary<Expression, Expression> substitues) {
-    Expressions = substitues;
-    codeSource = CodeSource.PredicateExtensions;
-  }
-  public ReplaceParameterExpressionVisitor(Dictionary<ParameterExpression, ParameterExpression> substitues) {
-    parameterExpressions = substitues;
-    codeSource = CodeSource.UniversalPredicateBuilder;
-  }
-
-  CodeSource codeSource;
-  private readonly Dictionary<ParameterExpression, ParameterExpression> parameterExpressions = new Dictionary<ParameterExpression, ParameterExpression>();
-  public Dictionary<Expression, Expression> Expressions { get; set; } = new Dictionary<Expression, Expression>();
-
-  enum CodeSource { PredicateExtensions, UniversalPredicateBuilder }
-
-  protected override Expression VisitParameter(ParameterExpression node) => codeSource switch {
-    CodeSource.PredicateExtensions => VisitParameter_PE(node),
-    CodeSource.UniversalPredicateBuilder => VisitParameter_UPB(node),
-    _ => throw new NotImplementedException()
-  };
-
-  Expression VisitParameter_PE(ParameterExpression node) => Expressions.TryGetValue(node, out Expression newValue) ? newValue : node;
-
-  Expression VisitParameter_UPB(ParameterExpression node) {
-    if (parameterExpressions.TryGetValue(node, out ParameterExpression replacement)) {
-      node = replacement;
-    }
-    return base.VisitParameter(node);
-  }
-
 }
