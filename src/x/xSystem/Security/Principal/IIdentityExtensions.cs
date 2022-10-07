bc11dc1279a43892f6f59b08cfa8cde4c7c6cc68
tb-mtg <tbeames@martogg.com.au>
2022-10-07 17:14:05 +1100

    Added IIdentityExtensions

--- src/x/xSystem/Security/Principal/IIdentityExtensions.cs
+++ src/x/xSystem/Security/Principal/IIdentityExtensions.cs
@@ -5,7 +5,7 @@
     public static string LogonUserName(this IIdentity identity) => identity.Name.Split('\\').LastOrDefault() ?? "unknown";
 
     public static bool IsAllowed(this IIdentity identity, string[] allowedUserNames, Exception? throwIfFalseException = null) {
-      var b = allowedUserNames.Contains(identity.LogonUserName(), StringComparer.OrdinalIgnoreCase);
+      bool b = allowedUserNames.Contains(identity.LogonUserName(), StringComparer.OrdinalIgnoreCase);
       if (!b && throwIfFalseException != null)
         //throw new PermissionDeniedException();
         throw throwIfFalseException;
@@ -13,7 +13,7 @@
     }
 
     public static string DebugString(this IIdentity identity, bool includeChildren = false) {
-      var sb = new StringBuilder();
+      StringBuilder sb = new StringBuilder();
       sb.AppendLine($"    .Identity");
       sb.AppendLine($"      .AuthenticationType: {identity.AuthenticationType}");
       sb.AppendLine($"      .Name: {identity.Name}");
