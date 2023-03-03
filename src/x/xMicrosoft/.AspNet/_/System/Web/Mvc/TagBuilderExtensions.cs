ce0cd743099a6bf308dac0630da44c150792c590
tb-mtg <tbeames@martogg.com.au>
2023-03-03 13:41:16 +1100

    Removed project xSystem.csproj and split into xSystem.NetFramework.csproj & xSystem.NetStandard.csproj

---
+++ src/x/xMicrosoft/.AspNet/_/System/Web/Mvc/TagBuilderExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace System.Web.Mvc {
+  public static class TagBuilderExtensions {
+
+    public static TagBuilder MergeAttributeIf(this TagBuilder tagBuilder, bool mergeIfTrue, string key, string value) {
+      if (mergeIfTrue) tagBuilder.MergeAttribute(key, value);
+      return tagBuilder;
+    }
+
+  }
+}
---
+++ src/x/xSystem/.NetFramework/_/System/Configuration/ConfigurationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Configuration;
+using System.Web.Configuration;
+
+namespace System.Configuration {
+  public static class ConfigurationExtensions {
+
+    public static KeyValueConfigurationCollection GetAppSettings(this Configuration configuration) => (KeyValueConfigurationCollection)(configuration.AppSettings?.Settings ?? ((object)new ConnectionStringSettingsCollection()));
+    public static ConnectionStringSettingsCollection GetConnectionStrings(this Configuration configuration) => configuration.ConnectionStrings?.ConnectionStrings ?? new ConnectionStringSettingsCollection();
+    public static NetSectionGroup GetSystemNet(this Configuration configuration) => (NetSectionGroup)(configuration.GetSectionGroup("system.net") ?? new NetSectionGroup());
+    public static MailSettingsSectionGroup GetSystemNetMailSettings(this Configuration configuration) => (MailSettingsSectionGroup)(configuration.GetSectionGroup("system.net/mailSettings") ?? new MailSettingsSectionGroup());
+    public static SmtpSection GetSystemNetMailSettingsSmtp(this Configuration configuration) => (SmtpSection)(configuration.GetSection("system.net/mailSettings/smtp") ?? new SmtpSection());
+    public static SystemWebSectionGroup GetSystemWeb(this Configuration configuration) => (SystemWebSectionGroup)(configuration.GetSectionGroup("system.web") ?? new SystemWebSectionGroup());
+  }
+}
