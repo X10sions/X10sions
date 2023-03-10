60918113eff1e204cafb69250a3cf48a357ba981
tb-mtg <tbeames@martogg.com.au>
2023-03-10 15:57:44 +1100

    Added typed extension methods for ICollection & IDictionary

--- src/x/xSystem/.NetStandard/Collections/Generic/ICollectionExtensions.cs
+++ src/x/xSystem/.NetStandard/Collections/Generic/ICollectionExtensions.cs
@@ -2,14 +2,16 @@
   public static class ICollectionExtensions {
 
     public static ICollection<T> AddIf<T>(this ICollection<T> collection, bool predicate, T item) {
-      if(predicate)
+      if (predicate)
         collection.Add(item);
       return collection;
     }
 
-    public static ICollection<T> AddIf<T>(this ICollection<T> collection, Func<bool> predicate, T item) => collection.AddIf<T>(predicate.Invoke(), item);
+    public static ICollection<T> AddIf<T>(this ICollection<T> collection, Func<bool> predicate, T item) => collection.AddIf(predicate.Invoke(), item);
 
-    public static ICollection<T> AddIf<T>(this ICollection<T> collection, Func<T, bool> predicate, T item) => collection.AddIf<T>(predicate.Invoke(item), item);
+    public static ICollection<T> AddIf<T>(this ICollection<T> collection, Func<T, bool> predicate, T item) => collection.AddIf(predicate.Invoke(item), item);
+
+    [Obsolete] public static bool IsEmptyOrNull<T>(this ICollection<T> collection, Func<T, bool> predicate, T item) => collection is null || collection.Count == 0;
 
   }
 }
---
+++ src/x/xSystem/.NetStandard/Collections/IDictionaryExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace System.Collections;
+public static class IDictionaryExtensions {
+
+  public static T? Get<T>(this IDictionary dictionary) => dictionary.Get<T>(typeof(T).FullName ?? typeof(T).Name);
+
+  public static T? Get<T>(this IDictionary dictionary, string key, T? defaultValue = default) {
+    if (dictionary is null) throw new ArgumentNullException(nameof(dictionary));
+    var value = dictionary[key];
+    return value is null || value is not T ? defaultValue : (T)value;
+  }
+
+  public static bool HasKey(this IDictionary dictionary, string key) => dictionary[key] != null;
+
+  public static void Set<T>(this IDictionary dictionary, T value) => dictionary.Set(typeof(T).FullName ?? typeof(T).Name, value);
+
+  public static T Set<T>(this IDictionary dictionary, string key, T value) {
+    if (dictionary is null) throw new ArgumentNullException(nameof(dictionary));
+    dictionary[key] = value;
+    return value;
+  }
+
+}
