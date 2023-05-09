4ce00264a62cf789fd5c0db2e09630f3b3df3baf
tb-mtg <tbeames@martogg.com.au>
2023-05-09 10:03:47 +1000

    Refactored to use System.ComponentModel ListSortDirection & DataObjectMethodType enums

---
+++ src/x/xSystem/.NetStandard/ComponentModel/DataObjectMethodTypeExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace System.ComponentModel;
+public static class DataObjectMethodTypeExtensions {
+  public static string Name(this DataObjectMethodType auditType) {
+    switch (auditType) {
+      case DataObjectMethodType.Delete: return nameof(DataObjectMethodType.Delete);
+      case DataObjectMethodType.Fill: return nameof(DataObjectMethodType.Fill);
+      case DataObjectMethodType.Insert: return nameof(DataObjectMethodType.Insert);
+      case DataObjectMethodType.Select: return nameof(DataObjectMethodType.Select);
+      case DataObjectMethodType.Update: return nameof(DataObjectMethodType.Update);
+      default: throw new ArgumentOutOfRangeException(nameof(auditType));
+    }
+  }
+
+  public static string Code(this DataObjectMethodType auditType) => auditType.Name().Substring(0, 1);
+
+  public static bool IsDelete(this DataObjectMethodType? o) => o == DataObjectMethodType.Delete;
+  public static bool IsInsert(this DataObjectMethodType? o) => o == DataObjectMethodType.Insert;
+  public static bool IsUpdate(this DataObjectMethodType? o) => o == DataObjectMethodType.Update;
+  public static bool IsSelect(this DataObjectMethodType? o) => o == DataObjectMethodType.Select;
+
+  public static DataObjectMethodType? GetSqlDmlAction(this bool isDirty, bool isNewRecord, bool doDelete)
+    => isDirty
+    ? doDelete
+       ? isNewRecord ? null : DataObjectMethodType.Delete
+       : isNewRecord ? DataObjectMethodType.Insert : DataObjectMethodType.Update
+    : null;
+
+}
