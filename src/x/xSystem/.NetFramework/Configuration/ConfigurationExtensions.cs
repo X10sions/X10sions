b2216a186fc5de25c3c546026cfaa5a7faf86639
tb-mtg <tbeames@martogg.com.au>
2023-08-04 15:59:10 +1000

    Added SectionGroup to ConfigurationExtensions

--- src/x/xSystem/.NetFramework/Configuration/ConfigurationExtensions.cs
+++ src/x/xSystem/.NetFramework/Configuration/ConfigurationExtensions.cs
@@ -3,11 +3,15 @@ using System.Web.Configuration;
 
 namespace System.Configuration {
   public static class ConfigurationExtensions {
+    static class SectionGroupNames {
+      public const string System_Net = "system.net";
+      public const string System_Web = "system.web";
+    }
     public static KeyValueConfigurationCollection GetAppSettings(this Configuration configuration) => (KeyValueConfigurationCollection)(configuration.AppSettings?.Settings ?? ((object)new ConnectionStringSettingsCollection()));
     public static ConnectionStringSettingsCollection GetConnectionStrings(this Configuration configuration) => configuration.ConnectionStrings?.ConnectionStrings ?? new ConnectionStringSettingsCollection();
-    public static NetSectionGroup GetSystemNet(this Configuration configuration) => (NetSectionGroup)(configuration.GetSectionGroup("system.net") ?? new NetSectionGroup());
-    public static MailSettingsSectionGroup GetSystemNetMailSettings(this Configuration configuration) => (MailSettingsSectionGroup)(configuration.GetSectionGroup("system.net/mailSettings") ?? new MailSettingsSectionGroup());
-    public static SmtpSection GetSystemNetMailSettingsSmtp(this Configuration configuration) => (SmtpSection)(configuration.GetSection("system.net/mailSettings/smtp") ?? new SmtpSection());
-    public static SystemWebSectionGroup GetSystemWeb(this Configuration configuration) => (SystemWebSectionGroup)(configuration.GetSectionGroup("system.web") ?? new SystemWebSectionGroup());
+    public static NetSectionGroup GetSystemNet(this Configuration configuration) => (NetSectionGroup)(configuration.GetSectionGroup(SectionGroupNames.System_Net) ?? new NetSectionGroup());
+    public static MailSettingsSectionGroup GetSystemNetMailSettings(this Configuration configuration) => configuration.GetSystemNet().MailSettings ?? new MailSettingsSectionGroup();
+    public static SmtpSection GetSystemNetMailSettingsSmtp(this Configuration configuration) => configuration.GetSystemNet().MailSettings.Smtp ?? new SmtpSection();
+    public static SystemWebSectionGroup GetSystemWeb(this Configuration configuration) => (SystemWebSectionGroup)(configuration.GetSectionGroup(SectionGroupNames.System_Web) ?? new SystemWebSectionGroup());
   }
 }
