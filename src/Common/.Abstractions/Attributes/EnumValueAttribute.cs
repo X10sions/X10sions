6bc4bad56fe7c965ddcc0ea2843d6efe5a8cc137
tb-mtg <tbeames@martogg.com.au>
2023-12-11 17:05:47 +1100

    Make EnumValueAttribute generic

--- src/Common/.Abstractions/Attributes/EnumValueAttribute.cs
+++ src/Common/.Abstractions/Attributes/EnumValueAttribute.cs
@@ -8,20 +8,22 @@ public class EnumValueAttribute<T> : Attribute {
   }
   public T Value { get; set; }
   public bool IsDefault { get; set; }
+
+  public static EnumValueAttribute<string> Create(string value, bool isDefault = false) => new(value, isDefault);
 }
 
-[AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
-public class EnumValueAttribute : EnumValueAttribute<object?> {
-  public EnumValueAttribute(object? value, bool isDefault = false) : base(value, isDefault) { }
-}
+//[AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
+//public class EnumValueAttribute : EnumValueAttribute<object?> {
+//  public EnumValueAttribute(object? value, bool isDefault = false) : base(value, isDefault) { }
+//}
 
-[AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
-public class EnumStringValueAttribute : EnumValueAttribute<string> {
-  public EnumStringValueAttribute(string value, bool isDefault = false) : base(value, isDefault) { }
-}
+//[AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
+//public class EnumStringValueAttribute : EnumValueAttribute<string> {
+//  public EnumStringValueAttribute(string value, bool isDefault = false) : base(value, isDefault) { }
+//}
 
 public static class EnumValueAttributeExtensions {
-  //public static string StringValue<T>(this T value) where T : Enum => value.GetCustomAttribute<EnumValueAttribute<string>>().Value;
+  public static string StringValue<T>(this T value) where T : Enum => value.GetCustomAttribute<EnumValueAttribute<string>>().Value;
   //public static IEnumerable<string> StringValues<T>(this T[] values) where T : Enum => values.Select(x => x.StringValue());
 
   //public static EnumValueAttribute GetEnumValueAttribute<T>(this T value) where T : Enum => value.GetCustomAttribute<EnumValueAttribute>();
