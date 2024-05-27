4e102acef9813c9e5f78b6555d35fb670c3f3c94
tb-mtg <tbeames@martogg.com.au>
2024-05-27 16:49:24 +1000

    GetEnumSelectList extension methods

---
+++ src/x/xMicrosoft/.AspNetCore/Mvc/ModelBinding/IModelMetadataProviderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Microsoft.AspNetCore.Mvc.ModelBinding;
+public static class IModelMetadataProviderExtensions {
+
+  public static IEnumerable<SelectListItem> GetEnumSelectList<TEnum>(this IModelMetadataProvider metadataProvider) where TEnum : struct => metadataProvider.GetEnumSelectList(typeof(TEnum));
+
+  public static IEnumerable<SelectListItem> GetEnumSelectList(this IModelMetadataProvider metadataProvider, Type enumType) {
+    ArgumentNullException.ThrowIfNull(enumType);
+    var metadata = metadataProvider.GetMetadataForType(enumType);
+    if (!metadata.IsEnum || metadata.IsFlagsEnum) {
+      var message = $"Resources.FormatHtmlHelper_TypeNotSupported_ForGetEnumSelectList({enumType.FullName}, {nameof(Enum).ToLowerInvariant()}, {nameof(FlagsAttribute)})";
+      throw new ArgumentException(message, nameof(enumType));
+    }
+    return metadata.GetEnumSelectList();
+  }
+
+}
---
+++ src/x/xMicrosoft/.AspNetCore/Mvc/ModelBinding/ModelMetadataExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Microsoft.AspNetCore.Mvc.ModelBinding;
+
+public static class ModelMetadataExtensions {
+
+  public static IEnumerable<SelectListItem> GetEnumSelectList(this ModelMetadata metadata) {
+    ArgumentNullException.ThrowIfNull(metadata);
+    if (!metadata.IsEnum || metadata.IsFlagsEnum) {
+      var message = $"Resources.FormatHtmlHelper_TypeNotSupported_ForGetEnumSelectList({metadata.ModelType.FullName}, {nameof(Enum).ToLowerInvariant()}, {nameof(FlagsAttribute)})";
+      throw new ArgumentException(message, nameof(metadata));
+    }
+    var selectList = new List<SelectListItem>();
+    var groupList = new Dictionary<string, SelectListGroup>();
+    foreach (var keyValuePair in metadata.EnumGroupedDisplayNamesAndValues) {
+      var selectListItem = new SelectListItem {
+        Text = keyValuePair.Key.Name,
+        Value = keyValuePair.Value,
+      };
+      if (!string.IsNullOrEmpty(keyValuePair.Key.Group)) {
+        if (!groupList.TryGetValue(keyValuePair.Key.Group, out var group)) {
+          group = new SelectListGroup() { Name = keyValuePair.Key.Group };
+          groupList[keyValuePair.Key.Group] = group;
+        }
+        selectListItem.Group = group;
+      }
+      selectList.Add(selectListItem);
+    }
+    return selectList;
+  }
+
+}
