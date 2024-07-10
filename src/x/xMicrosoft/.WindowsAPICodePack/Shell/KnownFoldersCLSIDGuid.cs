7c56ac813c1d1f5607fc00001fe14c3e68f0096d
tb-mtg <tbeames@martogg.com.au>
2024-07-10 12:42:22 +1000

    Deleted unused xMicrosoft.WindowsAPICodePack.csproj - Nuget package can now be used instead

--- src/x/xMicrosoft/.WindowsAPICodePack/Shell/KnownFoldersCLSIDGuid.cs
+++
@@ -1,8 +0,0 @@
-﻿
-
-namespace Microsoft.WindowsAPICodePack.Shell {
-  public static class KnownFoldersCLSIDGuid {
-    // CLSID GUID strings for relevant coclasses.
-    internal const string KnownFolderManager = "4df0c730-df9d-4ae3-9153-aa6b82e9795a";
-  }
-}
--- src/x/xMicrosoft/.WindowsAPICodePack/Taskbar/SetTabPropertiesOption.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Microsoft.WindowsAPICodePack.Taskbar {
-  public enum SetTabPropertiesOption {
-    None = 0x0,
-    UseAppThumbnailAlways = 0x1,
-    UseAppThumbnailWhenActive = 0x2,
-    UseAppPeekAlways = 0x4,
-    UseAppPeekWhenActive = 0x8
-  }
-}
