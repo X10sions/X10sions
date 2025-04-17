8648c0e59063389271d781ef3b914404e0d1df4b
tb-mtg <tbeames@martogg.com.au>
2025-04-17 12:23:36 +1000

    Started example using code from https://github.com/SimonCropp/Delta

--- src/.Examples/.Delta/Program.cs
+++ src/.Examples/.Delta/Program.cs
@@ -1,10 +1,13 @@
 using Delta;
+using Microsoft.Data.SqlClient;
+using Npgsql;
+using System.Collections.Generic;
 using System.Data.Common;
+using System.Reflection.Emit;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddScoped(_ => new SqlConnection(connectionString));
-builder.Services.AddScoped(_ => new NpgsqlConnection(connectionString));
-
+builder.Services.AddScoped(_ => new SqlConnection("connectionString"));
+builder.Services.AddScoped(_ => new NpgsqlConnection("connectionString"));
 
 var app = builder.Build();
 app.UseDelta();
@@ -14,37 +17,41 @@ app.MapGet("/", () => "Hello World!");
 app.MapGroup("/group")    .UseDelta()    .MapGet("/", () => "Hello Group!");
 
 app.UseDelta(
-    shouldExecute: httpContext => {
-      var path = httpContext.Request.Path.ToString();
-      return path.Contains("match");
-    });
+  //getConnection: httpContext => httpContext.RequestServices.GetRequiredService<SqlConnection>(),
+  getConnection: httpContext => {
+    var provider = httpContext.RequestServices;
+    var connection = provider.GetRequiredService<SqlConnection>();
+    var transaction = provider.GetService<SqlTransaction>();
+    return new(connection, transaction);
+  },
+  shouldExecute: httpContext => {
+    var path = httpContext.Request.Path.ToString();
+    return path.Contains("match");
+  });
 
 app.Run();
 
-
-
-
-static void InitConnectionTypes() {
+static void InitConnectionTypes(Type connectionType, Type transactionType) {
   var sqlConnectionType = Type.GetType("Microsoft.Data.SqlClient.SqlConnection, Microsoft.Data.SqlClient");
   if (sqlConnectionType != null) {
     connectionType = sqlConnectionType;
     transactionType = sqlConnectionType.Assembly.GetType("Microsoft.Data.SqlClient.SqlTransaction")!;
     return;
   }
-
   var npgsqlConnection = Type.GetType("Npgsql.NpgsqlConnection, Npgsql");
   if (npgsqlConnection != null) {
     connectionType = npgsqlConnection;
     transactionType = npgsqlConnection.Assembly.GetType("Npgsql.NpgsqlTransaction")!;
     return;
   }
-
   throw new("Could not find connection type. Tried Microsoft.Data.SqlClient.SqlConnection and Npgsql.NpgsqlTransaction");
 }
 
-static Connection DiscoverConnection(HttpContext httpContext) {
+static Connection DiscoverConnection(HttpContext httpContext, Type connectionType, Type transactionType) {
   var provider = httpContext.RequestServices;
   var connection = (DbConnection)provider.GetRequiredService(connectionType);
   var transaction = (DbTransaction?)provider.GetService(transactionType);
   return new(connection, transaction);
 }
+
+
