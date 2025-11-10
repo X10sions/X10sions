40dce559b3bd94400e8265a637808560b3e920e9
tb-mtg <tbeames@martogg.com.au>
2025-11-10 20:33:04 +1100

    Refactor ChangeAction enum formatting
    Refactored the `ChangeAction` enumeration in the `System.Data.Linq` namespace to adjust formatting. Enumeration values (`None = 0`, `Delete = 1`, `Insert = 2`, `Update = 3`) were removed and re-added without altering functionality. This change improves code consistency and style.

--- src/x/xSystem/.Data/.Linq/ChangeAction.cs
+++ src/x/xSystem/.Data/.Linq/ChangeAction.cs
@@ -1,9 +1,7 @@
 ﻿namespace System.Data.Linq;
 public enum ChangeAction {
-    None = 0,
-    Delete = 1,
-    Insert = 2,
-    Update = 3
-}
-
+  None = 0,
+  Delete = 1,
+  Insert = 2,
+  Update = 3
 }
